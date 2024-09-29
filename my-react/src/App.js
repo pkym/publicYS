abd1d3c22b71a2f16cac162492f661a0fc9a695a
leesangdal <asksa1256@gmail.com>
2024-09-29 18:50:56 +0900

    레이아웃 컴포넌트 분리 (메인, 왼쪽/오른쪽 섹션, 메인 인포 리스트, 아이콘 리스트)

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,37 +1,12 @@
-import './App.css';
-import './styles/main.css'
-import SafeText from './components/SafeText';
-import LeftSection from './components/LeftSection';
-import IconList from './components/RightSection';
+import "./styles/reset.css";
+import "./styles/main.css";
+import MainPage from "./components/MainPage";
 
 function App() {
-  
-  const today = new Date().toString().padStart(2, '0');;
-
+  const today = new Date().toString().padStart(2, "0");
   console.log(today);
-  return (
-    <div className="background">
-      <LeftSection/>
-      <section className="right">
-        
-        <h2>재난보관함</h2>
-        <div className="content">
-          <h4>대피요령</h4>
-          <div className="icon-box">
-            <IconList/>
-          </div>
-          <h4>최근재난문자</h4>
-          <div>
-            {/* <SafeText pageNo='1' numOfRows='3'/> */}
-          </div>
-          <div>
 
-          </div>
-        
-        </div>
-      </section>
-    </div>
-  );
-};
+  return <MainPage />;
+}
 
 export default App;
--- my-react/src/components/RightSection.jsx
+++ my-react/src/components/RightSection.jsx
@@ -1,37 +1,18 @@
-import earthquakeImg from '../assets/icon/earthquake.png'
-import floodedImg from '../assets/icon/flooded.png'
-import heilImg from '../assets/icon/heil.png'
-import typoonImg from '../assets/icon/typoon.png'
-import burningImg from '../assets/icon/burning.png'
-import avalancheImg from '../assets/icon/avalanche.png'
-import explosionImg from '../assets/icon/explosion.png'
-import snowyImg from '../assets/icon/snowy.png'
-import houImg from '../assets/icon/hou.png'
-import collapseImg from '../assets/icon/collapse.png'
+import logoImg from "../assets/img/logo_wt.png";
+import IconList from "./IconList";
 
-
-const ICONS = [
-    {image: earthquakeImg, title:"지진"}, {image: floodedImg, title:"홍수"}, {image: heilImg,  title:"해일"}, {image: typoonImg,  title:"태풍"}, {image: burningImg,  title:"화재"}
-    , {image: avalancheImg,  title:"산사태"}, {image: explosionImg,  title:"폭발"}, {image: snowyImg,  title:"대설"}, {image: houImg,  title:"호우"}, {image: collapseImg,  title:"붕괴"}
-];
-
-function Icons({ image, title}) {
-    return (
-      <>
-        <img src={image} alt="..." />
-        <span>{title}</span>        
-      </>
-    );
-  }
-  
-
-export default function IconList(){
-    return(
-        <>
-            {ICONS.map((item)=>(
-                        <button><Icons key={item.title} {...item}/></button>
-        ))}
-        </>
-  
-      )
+export default function RightSection() {
+  return (
+    <section className="right">
+      <h2 className="logo">
+        <img src={logoImg} alt="재난 보관함" />
+      </h2>
+      <div className="content">
+        <h4>대피요령</h4>
+        <IconList />
+        <h4>최근재난문자</h4>
+        <div>{/* <SafeText pageNo='1' numOfRows='3'/> */}</div>
+      </div>
+    </section>
+  );
 }
