1fa12986a45dde1e9a62a618546c7ae8351a5c7c
asksa1256@gmail.com <asksa1256@gmail.com>
2024-10-01 12:57:10 +0900

    메인 - 긴급 대피소 api 연동

--- my-react/src/components/EmergencyShelterList.jsx
+++ my-react/src/components/EmergencyShelterList.jsx
@@ -1,70 +1,46 @@
 import {useState, useEffect} from 'react';
+import EmergencyShelterItem from './EmergencyShelterItem';
 
 // Provided API key and parameters
-const apiKey = 'ST9W4WW508Z6XV06';
-// const pageNo = '1'; //페이지번호
-// const numOfRows = '100'; //페이지당 개수
-// const crtDt = '20240101'; //조회시작일자
-// const rgnNm = '1'; //지역명
+const apiKey = 'Z67lDD5584KUF%2BGcDc2iij53yVrZ48k9IF62W7qZi0cg0N8tQz4EECztBTN3YhCbQN8j4VdztRhe3LEIKpyveQ%3D%3D';
+const pageNo = '1'; //페이지번호
+const numOfRows = '5'; //페이지당 개수
 
 // API endpoint
 
 export default function EmergencyShelterList({}){
-  // const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
   
-  // const url = `safeText/V2/api/DSSP-IF-00247?serviceKey=${apiKey}&pageNo=${pageNo}&numOfRows=${numOfRows}`;
+  const url = `https://apis.data.go.kr/1741000/TsunamiShelter4/getTsunamiShelter4List?serviceKey=${apiKey}&pageNo=${pageNo}&numOfRows=${numOfRows}&type=json`
 
-  // useEffect(() => {
-//     fetch(url)
-//     .then((response) => {
-//         console.log(response)
-//         if (!response.ok) {
-//         throw new Error(`Error: ${response.status} ${response.statusText}`);
-//         }
-//         return response.json(); 
-//     })
-//     .then((data) => {
-//         setData(data.body);
-//     })
-//     .catch((error) => {
-//         console.log("error:"+error)
-//     });
-  // }, [url]);
+  useEffect(() => {
+    fetch(url)
+    .then((response) => {
+        console.log(response)
+        if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+        return response.json(); 
+    })
+    .then((data) => {
+        setData(data.TsunamiShelter[1].row);
+    })
+    .catch((error) => {
+        console.log("error:"+error)
+    });
+  }, [url]);
   
   return (
     <>
-      {/* {data ? 
-        // Display fetched data
+      {data ?
         (
             Object.values(data).map((values)=>(
-                <SafeTextItem params={values}/>
+                <EmergencyShelterItem params={values}/>
             ))
         ) : (
-            <p>Loading...</p> // Show loading message while fetching
+            <p>Loading...</p>
         )
-      } */}
-      <li>
-        <div className="list-header">
-          <span className="chip">민방위대피시설</span>
-        </div>
-        <h3 className='title'>
-          대방아파트 지하 1층 대피소
-        </h3>
-        <h6 className='desc'>
-          전라남도 나주시 대호길 12-5
-        </h6>
-      </li>
-      <li>
-        <div className="list-header">
-          <span className="chip">공원</span>
-        </div>
-        <h3 className='title'>
-          학여울근린공원
-        </h3>
-        <h6 className='desc'>
-          서울특별시 강남구 개포로 509
-        </h6>
-      </li>
+      }
     </>
   )
 }
