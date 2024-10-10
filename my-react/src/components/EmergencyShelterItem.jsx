aea0d9f64107d563a97f2169ddc8459c0630b901
leesangdal <asksa1256@gmail.com>
2024-10-10 13:28:55 +0900

    메인 - 긴급 대피소 지역별 api 연동

--- my-react/src/components/EmergencyShelterItem.jsx
+++ my-react/src/components/EmergencyShelterItem.jsx
@@ -1,20 +1,18 @@
-export default function EmergencyShelterItem({params}){
-  return(
+export default function EmergencyShelterItem({ params }) {
+  return (
     <>
       <li>
         <div className="list-header">
-          <span className="chip">{params.shel_div_type}</span>
-          <span className="chip">{params.shel_av}명까지</span>
-          <span className="chip">{params.seismic === "" ? "내진적용X" : "내진적용O"}</span>
+          <span className="chip">{params.ACMD_FCLTY_SE_CD}</span>
+          <span className="chip">{params.VT_ACMD_PSBL_NMPR}명 수용</span>
+          <span className="chip">
+            {params.USE_SE_CD === "Y" ? "내진적용X" : "내진적용"}
+          </span>
         </div>
-        <h3 className='title'>
-          {params.shel_nm}
-        </h3>
-        <h6 className='desc'>
-          {params.address}
-        </h6>
-        <p className="tel">문의: {params.manage_gov_nm} {params.tel}</p>
+        <h3 className="title">{params.VT_ACMD_FCLTY_NM}</h3>
+        <h6 className="desc">{params.DTL_ADRES}</h6>
+        <p className="tel">문의: {params.TEL_NO}</p>
       </li>
     </>
-  )
+  );
 }
