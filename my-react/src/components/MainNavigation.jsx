a3e3d66754062c1dc43fe35a0374e620b60912d6
asksa1256@gmail.com <asksa1256@gmail.com>
2024-11-13 20:21:49 +0900

    gnb active 처리 방식 변경

--- my-react/src/components/MainNavigation.jsx
+++ my-react/src/components/MainNavigation.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import homeActiveImg from "../assets/icon/home-fill.svg"
 import homeImg from "../assets/icon/home-line.svg"
 import safeTextImg from "../assets/icon/archive-stack-line.svg"
@@ -12,28 +12,28 @@ import peopleImg from "../assets/icon/group-line.svg"
 import peopleActiveImg from "../assets/icon/group-fill.svg"
 
 export default function MainNavigation() {
-  const [isActive, setIsActive] = useState("home");
+  const location = useLocation();
 
   return (
     <nav className="dockbar">
-      <NavLink to="/" onClick={() => setIsActive("home")}>
-        <img src={isActive === "home" ? homeActiveImg : homeImg} alt=""/>
+      <NavLink to="/">
+        <img src={location.pathname === "/" ? homeActiveImg : homeImg} alt=""/>
         <span>메인</span>
       </NavLink>
-      <NavLink to="/safeText" onClick={() => setIsActive("safeText")}>
-        <img src={isActive === "safeText" ? safeTextActiveImg : safeTextImg} alt=""/>
+      <NavLink to="/safeText">
+        <img src={location.pathname === "/safeText" ? safeTextActiveImg : safeTextImg} alt=""/>
         <span>재난 문자</span>
       </NavLink>
-      <NavLink to="/shelterInfo" onClick={() => setIsActive("shelterInfo")}>
-        <img src={isActive === "shelterInfo" ? runActiveImg : runImg} alt=""/>
+      <NavLink to="/shelterInfo">
+        <img src={location.pathname === "/shelterInfo" ? runActiveImg : runImg} alt=""/>
         <span>대피소 정보</span>
       </NavLink>
-      <NavLink to="/escapeTip" onClick={() => setIsActive("escapeTip")}>
-        <img src={isActive === "escapeTip" ? alarmActiveImg : alarmImg} alt=""/>
+      <NavLink to="/escapeTip">
+        <img src={location.pathname === "/escapeTip" ? alarmActiveImg : alarmImg} alt=""/>
         <span>대피 요령</span>
       </NavLink>
-      <NavLink to="/liveChat" onClick={() => setIsActive("liveChat")}>
-        <img src={isActive === "liveChat" ? peopleActiveImg : peopleImg} alt=""/>
+      <NavLink to="/liveChat">
+        <img src={location.pathname === "/liveChat" ? peopleActiveImg : peopleImg} alt=""/>
         <span>실시간 대화</span>
       </NavLink>
     </nav>
