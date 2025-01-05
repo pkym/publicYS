b225f5de8b179c38f7c9ebd3c8da49be0966989b
leesangdal <asksa1256@gmail.com>
2025-01-05 19:22:25 +0900

    재난 문자 - 발송 지역별 조회

--- my-react/src/components/SafeText/SafeTextMoreList.jsx
+++ my-react/src/components/SafeText/SafeTextMoreList.jsx
@@ -4,12 +4,25 @@ import SafeTextItem from "./SafeTextItem";
 const apiKey = "ST9W4WW508Z6XV06";
 
 export default function SafeTextMoreList(props) {
+  const [sidoData, setSidoData] = useState([]);
+  const [sidoName, setSidoName] = useState("");
   const [data, setData] = useState([]);
-  const [emptyData, setEmptyData] = useState(false);
 
-  const url = `safeText/V2/api/DSSP-IF-00247?serviceKey=${apiKey}&pageNo=${props.pageNo}&numOfRows=${props.numOfRows}&crtDt=${props.date}`;
+  // 전체 지역 조회 url
+  const url = `safeText/V2/api/DSSP-IF-00247?serviceKey=${apiKey}&pageNo=${props.pageNo}&crtDt=${props.date}`;
 
-  useEffect(() => {
+  // 발송 지역별 url
+  const sendingAreaUrl = `safeText/V2/api/DSSP-IF-00247?serviceKey=${apiKey}&pageNo=1&crtDt=${props.date}&rgnNm=${sidoName}`;
+
+  function setSidoNameHandler(e) {
+    setSidoName(e.target.selectedOptions[0].text);
+  }
+
+  function searchSafeTextHandler() {
+    sidoName !== "발송 지역" ? fetchByRegionData() : fetchData();
+  }
+
+  function fetchData() {
     fetch(url)
       .then((response) => {
         if (!response.ok) {
@@ -21,38 +34,111 @@ export default function SafeTextMoreList(props) {
         if (
           data.header.resultMsg ===
           "LIMITED NUMBER OF SERVICE REQUESTS EXCEEDS ERROR"
-        ) {setData(data.header.resultMsg);} 
-        else {
+        ) {
+          setData(data.header.resultMsg);
+        } else {
           setData(data.body);
-          if (data.length === 0) {
-            setEmptyData(true);
-          } else {
-            setEmptyData(false);
-          }
         }
       })
       .catch((error) => {
         console.log("error:" + error);
       });
+  }
+
+  function fetchByRegionData() {
+    fetch(sendingAreaUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (
+          data.header.resultMsg ===
+          "LIMITED NUMBER OF SERVICE REQUESTS EXCEEDS ERROR"
+        ) {
+          setData(data.header.resultMsg);
+        } else {
+          setData(data.body);
+        }
+      })
+      .catch((error) => {
+        console.log("error:" + error);
+      });
+  }
+
+  function selectSidoHandler() {
+    const sidoUrl =
+      "shelter/idsiSFK/neo/ext/json/arcd/bd/bd_sido.json?_=1728528610168";
+    fetch(sidoUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setSidoData(data);
+      })
+      .catch((error) => {
+        console.log("error:" + error);
+      });
+  }
+
+  useEffect(() => {
+    fetchData();
+    selectSidoHandler(); // 초기 렌더링 시 전체 시,도 데이터 불러오기
   }, []);
 
   return (
     <>
-      {/* {data === "LIMITED NUMBER OF SERVICE REQUESTS EXCEEDS ERROR" ? (
-        <p>API 요청 일일 제한 횟수를 초과했습니다.</p>
-      ) : !emptyData ? (
-        Object.values(data).map((values) => (
-          <SafeTextItem params={values} key={values.SN} />
-        ))
-      ) : (
-        <p>데이터가 없습니다.</p>
-      )} */}
-      {data ? Object.values(data).map((values) => (
-          <SafeTextItem params={values} key={values.SN} />
-        ))
-       : (
-        <p>데이터가 없습니다.</p>
-      )}
+      <div className="form-wrap">
+        <div className="form-selects">
+          <select
+            className="form-select"
+            name="sido"
+            id="sido"
+            onChange={setSidoNameHandler}
+            onClick={(e) => (e.target.options[0].text = "발송 지역")}
+          >
+            <option value="1">발송 지역</option>
+            {sidoData.map((data) => (
+              <option key={data.SIDO_CD} value={data.SIDO_CD}>
+                {data.SIDO_NM}
+              </option>
+            ))}
+          </select>
+          <select className="form-select" name="disasterCtg" id="disasterCtg">
+            <option value="1">재난 유형</option>
+          </select>
+        </div>
+        <button
+          type="button"
+          className="btn-search"
+          onClick={searchSafeTextHandler}
+        >
+          검색
+        </button>
+      </div>
+      <ul className="safe-text-ul">
+        {/* {data === "LIMITED NUMBER OF SERVICE REQUESTS EXCEEDS ERROR" ? (
+          <p>API 요청 일일 제한 횟수를 초과했습니다.</p>
+        ) : !emptyData ? (
+          Object.values(data).map((values) => (
+            <SafeTextItem params={values} key={values.SN} />
+          ))
+        ) : (
+          <p>데이터가 없습니다.</p>
+        )} */}
+        {data ? (
+          Object.values(data).map((values) => (
+            <SafeTextItem params={values} key={values.SN} />
+          ))
+        ) : (
+          <p>데이터가 없습니다.</p>
+        )}
+      </ul>
     </>
   );
 }
--- my-react/src/pages/SafeTextPage.jsx
+++ my-react/src/pages/SafeTextPage.jsx
@@ -1,81 +1,16 @@
-import React, {useState, useEffect} from 'react';
+import React from "react";
 import SafeTextMoreList from "../components/SafeText/SafeTextMoreList";
-import getToday from '../components/util/date';
+import getToday from "../components/util/date";
 
-export default function SafeTextPage() {
+export default function SafeTextPage(props) {
   const todayDate = getToday();
-  const [sidoData, setSidoData] = useState([]);
-  const [sidoCode, setSidoCode] = useState("");
-
-  function searchSafeTextHandler() {
-    console.log("test")
-  }
-
-  // 발송 지역 api
-  // const url = `safeText/V2/api/DSSP-IF-00247?serviceKey=${apiKey}&pageNo=${props.pageNo}&numOfRows=${props.numOfRows}&crtDt=${props.date}`;
-
-  // useEffect(() => {
-  //   fetch(url)
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error(`Error: ${response.status} ${response.statusText}`);
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       if (
-  //         data.header.resultMsg ===
-  //         "LIMITED NUMBER OF SERVICE REQUESTS EXCEEDS ERROR"
-  //       ) {setData(data.header.resultMsg);} 
-  //       else {
-  //         setData(data.body);
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.log("error:" + error);
-  //     });
-  // }, []);
 
   return (
     <>
       <h2>재난 문자</h2>
       <div className="safeText-wrap">
-        <div className="form-wrap">
-          <div className="form-selects">
-            <select
-              className="form-select"
-              name="sido"
-              id="sido"
-              // onChange={getSidoCodeHandler}
-              onClick={e => e.target.options[0].text = "발송 지역"}
-            >
-              {/* <option value="1">{shelterCtxDataOps ? shelterCtxDataOps[0] : "발송 지역"}</option> */}
-              {sidoData.map((data) => (
-                <option key={data.SIDO_CD} value={data.SIDO_CD}>
-                  {data.SIDO_NM}
-                </option>
-              ))}
-            </select>
-            <select className="form-select" name="disasterCtg" id="disasterCtg">
-              <option value="1">재난 유형</option>
-            </select>
-          </div>
-          <button
-            type="button"
-            className="btn-search"
-            onClick={searchSafeTextHandler}
-          >
-            검색
-          </button>
-        </div>
-        <ul className="safe-text-ul">
-          <SafeTextMoreList
-            pageNo="1"
-            numOfRows="30"
-            date={todayDate}
-          />
-        </ul>
+        <SafeTextMoreList pageNo="1" numOfRows="30" date={todayDate} />
       </div>
     </>
-  )
+  );
 }
